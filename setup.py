b5f6aa16f049cdbbe3249b237d3c9493f5032ab7
Vitalii <witalikhomyn@gmail.com>
2025-12-30 21:36:28 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,42 @@
-from setuptools import setup, Extension
+import sys
+from setuptools import setup, Extension, find_packages
 
 module = Extension(
     'minicov_tracer',
-    sources=['src/tracer.c']
+    sources=['src/tracer.c'],
+    optional=True,  # fallback for C compilation fails
 )
+
+# dependencies
+install_requires = []
+if sys.version_info < (3, 11):
+    install_requires.append("tomli")
 
 setup(
     name='minicov',
-    version='1.0',
-    description='MiniCoverage with C Tracer',
-    ext_modules=[module]
+    version='1.0.0',
+    description='A minimalist, high-performance (I hope) code coverage tool with MC/DC support and plans for further expansion.',
+    author='Vitaliy Khomyn',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    ext_modules=[module],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'minicov=src.main:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires='>=3.10',
 )
